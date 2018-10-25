2fd4bfab9be31f5321e7487d11e0f0079444e1f4
Kelsey Chan <kelseykchan@gmail.com>
2018-10-24 23:07:44 -0400

    Fix minor bug with centering

--- src/client/gallery_container.js
+++ src/client/gallery_container.js
@@ -82,6 +82,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexWrap: 'wrap',
     maxWidth: 850,
+    margin: 'auto',
     paddingTop: 20,
     paddingLeft: 20,
     paddingRight: 20,
